d9fec1bbf18d50f2a454bc3e83b2042df581ba90
Andy Weidenbaum <archbaum@gmail.com>
2019-04-09 15:39:17 +1000

    authorize_scan.sh: finesse

--- authorize_scan.sh
+++ authorize_scan.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 main() {
-  local idProduct
-  local idVendor
+  local _id_product
+  local _id_vendor
+  local _usb_whitelist_path
+
+  _usb_whitelist_path="/etc/usb_whitelist"
 
   cd /sys/bus/usb/devices/ \
     || exit 1
 
-  grep -v : -- * | while read -r device; do
-    idProduct="$(cat "$device/idProduct")"
-    idVendor="$(cat "$device/idVendor")"
-    grep "$idVendor:$idProduct" /etc/usb_whitelist \
-      && echo 1 > "$device/authorized"
+  grep -v : -- * | while read -r _device; do
+    _id_product="$(cat "$_device/idProduct")"
+    _id_vendor="$(cat "$_device/idVendor")"
+    grep "$_id_vendor:$_id_product" "$_usb_whitelist_path" \
+      && echo 1 > "$_device/authorized"
   done
 }
 
