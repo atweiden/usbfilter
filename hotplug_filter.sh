8b0f55ac2a9725f826c81d63080deaf848111b26
Andy Weidenbaum <archbaum@gmail.com>
2019-04-09 15:47:16 +1000

    hotplug_filter.sh: finesse

--- hotplug_filter.sh
+++ hotplug_filter.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 main() {
-  env >> /tmp/hotpluglog
+  local _log_hotplug_path
+  local _search_path
+  local _usb_whitelist_path
+  local _denied_log_path
+
+  local _port
+  local _id_product
+  local _id_vendor
+  local _b_interface_class
+  local _search
+
+  _log_hotplug_path="/tmp/hotpluglog"
+  _search_path="/tmp/search"
+  _usb_whitelist_path="/etc/usb_whitelist"
+  _denied_log_path="/tmp/denied_log"
+
+  env >> "$_log_hotplug_path"
 
   echo "$SUBSYSTEM"
 
@@ -13,32 +29,29 @@ main() {
     [[ "$DEVTYPE" == "usb_interface" ]] \
       && (cd "/sys/$DEVPATH/.." || exit 1)
 
-    port="$(basename "$DEVPATH")"
-    idProduct="$(cat idProduct)"
-    idVendor="$(cat idVendor)"
-    bInterfaceClass=""
-    search=""
+    _port="$(basename "$DEVPATH")"
+    _id_product="$(cat idProduct)"
+    _id_vendor="$(cat idVendor)"
 
-    [[ "$DEVTYPE" == "usb_device" ]] \
-      && search="$idVendor:$idProduct"
+    if [[ "$DEVTYPE" == "usb_device" ]]; then
+      _search="$_id_vendor:$_id_product"
+    fi
 
     if [[ "$DEVTYPE" == "usb_interface" ]]; then
       (cd "/sys/$DEVPATH/" || exit 1)
-      bInterfaceClass="$(cat bInterfaceClass)"
-      search="$idVendor:$idProduct:$bInterfaceClass"
+      _b_interface_class="$(cat bInterfaceClass)"
+      _search="$_id_vendor:$_id_product:$_b_interface_class"
     fi
 
-    echo "$search" >> /tmp/search
+    echo "$_search" >> "$_search_path"
 
-    if [[ "$(grep -q "$search" /etc/usb_whitelist)" -ne 0 ]]; then
-      echo "denied $DEVPATH" >> /tmp/denied_log
-      exit
-    fi
+    grep -q "$_search" "$_usb_whitelist_path" \
+      || (echo "denied $DEVPATH" >> "$_denied_log_path"; exit)
 
     [[ -e "/sys/$DEVPATH/authorized" ]] \
       && echo 1 > "/sys/$DEVPATH/authorized"
 
-    echo "$port" > /sys/bus/usb/drivers_probe
+    echo "$_port" > /sys/bus/usb/drivers_probe
   fi
 }
 
